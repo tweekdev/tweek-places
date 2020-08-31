6bd5efdab2de2998fd6683360d3dbec84d45c4ef
tweekdev <tja.theau54@gmail.com>
2020-08-31 21:08:46 +0200

    first commit

---
+++ src/places/pages/NewPlace.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Input from "../../shared/components/FormElements/Input";
+import "./NewPlace.css";
+const NewPlace = () => {
+  return (
+    <form className='place-form'>
+      <Input element='input' type='text' label='Title'></Input>
+    </form>
+  );
+};
+
+export default NewPlace;
---
+++ src/shared/components/Navigation/MainNavigation.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import Backdrop from "../UIElements/Backdrop";
+import MainHeader from "./MainHeader";
+import "./MainNavigation.css";
+import NavLinks from "./NavLinks";
+import SideDrawer from "./SideDrawer";
+const MainNavigation = () => {
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+  const openDrawerHandler = () => {
+    setDrawerIsOpen(true);
+  };
+
+  const closeDrawerHandler = () => {
+    setDrawerIsOpen(false);
+  };
+  return (
+    <>
+      {drawerIsOpen && <Backdrop onClick={closeDrawerHandler}></Backdrop>}
+      <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
+        <nav className='main-navigation__drawer-nav'>
+          <NavLinks></NavLinks>
+        </nav>
+      </SideDrawer>
+      <MainHeader>
+        <button
+          className='main-navigation__menu-btn'
+          onClick={openDrawerHandler}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+        <h1 className='main-navigation__title'>
+          <Link to='/'>YourPlaces</Link>
+        </h1>
+        <nav className='main-navigation__header-nav'>
+          <NavLinks></NavLinks>
+        </nav>
+      </MainHeader>
+    </>
+  );
+};
+
+export default MainNavigation;
